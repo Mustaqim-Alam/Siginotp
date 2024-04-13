40ba4a65ec851f41fc3886cb06fabe0d6a6ba918
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-04-13 22:35:16 +0530

    Alam commmit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import Otp from './pages/Otp';
-import Error from './pages/Error';
-import Headers from './components/Headers';
-import { Routes, Route } from "react-router-dom"
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css'; // You may remove this line if you don't need it
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Headers from "./components/Headers";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Otp from "./pages/Otp";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -14,11 +15,11 @@ function App() {
       <Headers />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/user/otp' element={<Otp />} />
-          <Route path='*' element={<Error />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/user/otp" element={<Otp />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </div>
     </div>
